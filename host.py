48e2d948c152cbdedb4c0cfc3cd3e4268eb134c8
Nicholas Petty <nick@ihackeverything.com>
2016-03-14 01:06:36 -0500

    remote 857d files
    sudo code

---
+++ host.py
@@ -0,0 +1,40 @@
+# Sockets #
+# - Panel Receive
+# - Main Send
+# - PTT
+# - PWR
+# - Data Recieve
+# - Data Send
+# - Voice Recieve
+# - Audio Send
+
+
+def panelRecieve():
+	# listen for web data from panel and pass to serial write 1
+
+def mainSend():
+	# listen for serial data from main and pass to remote via web
+
+def ptt():
+	# listen for web data to trigger relay
+
+def pwr():
+	# listen for web data to trigger relay (pin to gnd)
+
+def dataReceive():
+	# listen for web data from remote com port and pass to serial write 2
+
+def dataSend():
+	# listen on com port 2 and relay to remote via web
+
+def voiceRecieve():
+	# listen to remote audio stream, pass out of sound card to main unit
+
+def audioSend():
+	# stream audio to remote device
+
+
+def main():
+	# create each socket in sub process as they are requested
+	# remote connects back to host. Calling for a certain socket.  Socket is created and connection
+	# is established.  remote then calls for another specific socket.
---
+++ remote.py
@@ -0,0 +1,20 @@
+# Sockets #
+# - Main Send
+# - Panel Recieve
+# - PTT
+# - PWR
+# - Data Send
+# - Data Recieve
+# - Voice Send
+# - Audio Recieve
+
+def main():
+	# start host panelRecieve()
+	# 	panelRecieve hosts a socket
+	# start mainSend() 
+	# 	mainSend starts a socket and trys to connect to host panelReceive
+	
+	# start host MainSend()
+	#	mainSend hosts a socket
+	# start panelRecieve()
+	# 	panelRecieve creates a socket and trys to connect to host mainSend
